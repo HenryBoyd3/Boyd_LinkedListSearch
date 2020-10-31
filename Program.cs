c612bf7dc8cb44c64f783c8f87152c8cb87c7e25
Dorkwoodgames <dorkwoodgames@gmail.com>
2020-10-30 19:06:47 -0700

    added a search by gender option to the list

--- Program.cs
+++ Program.cs
@@ -18,17 +18,18 @@ namespace Boyd_LinkedListSearch
 
                 Console.WriteLine();
                 Console.WriteLine("1.Search for a Name");
-                Console.WriteLine("2.See total number of names");
-                Console.WriteLine("3.See total number of male names");
-                Console.WriteLine("4.See total number of female names");
-                Console.WriteLine("5.add a name");
-                Console.WriteLine("6.see most popular male name");
-                Console.WriteLine("7.see most popular female name");
-                Console.WriteLine("8.exit");
+                Console.WriteLine("2.Search for a Name with gender");
+                Console.WriteLine("3.Add a name");
+                Console.WriteLine("4.See most popular female name");
+                Console.WriteLine("5.See most popular male name");
+                Console.WriteLine("6.See total number of names");
+                Console.WriteLine("7.See total number of female names");
+                Console.WriteLine("8.See total number of Male names");
+                Console.WriteLine("9.exit");
                 userInput = parseinput();
                 UserOptions(userInput, programList,sw);
                 Console.WriteLine();
-                if (userInput == 8)
+                if (userInput == 9)
                     break;
             }
         }
@@ -44,24 +45,28 @@ namespace Boyd_LinkedListSearch
                     searchName(List, timer);
                     break;
                 case 2:
-                    Console.WriteLine(List.getTotalNodes());
+                    Console.WriteLine("please enter the name you would like to search for.");
+                    searchNameAndGender(List, timer);
                     break;
                 case 3:
-                    Console.WriteLine(List.getTotalMaleNodes());
-                    break;
-                case 4:
-                    Console.WriteLine(List.getTotalFemaleNodes());
-                    break;
-                case 5:
                     userAddName(List, timer);
                     break;
-                case 6:
+                case 4:
+                    Console.WriteLine(List.getMostPopFemale()); 
+                    break;
+                case 5:
                     Console.WriteLine(List.getMostPopMale());
                     break;
+                case 6:
+                    Console.WriteLine(List.getTotalNodes());
+                    break;
                 case 7:
-                    Console.WriteLine(List.getMostPopFemale()) ;
+                    Console.WriteLine(List.getTotalFemaleNodes());
                     break;
                 case 8:
+                    Console.WriteLine(List.getTotalMaleNodes());
+                    break;
+                case 9:
                     Console.WriteLine();
                     break;
 
@@ -79,6 +84,7 @@ namespace Boyd_LinkedListSearch
                 Console.WriteLine(name + " was not found in our list would you like to add it? \n if so press Y");
                 if (Console.ReadKey().Key == ConsoleKey.Y)
                 {
+                    Console.WriteLine();
                     userAddName(List, name,timer);
                 }
             }
@@ -89,7 +95,34 @@ namespace Boyd_LinkedListSearch
                 Console.WriteLine(nameData);
             }
             
-        } 
+        }
+        public static void searchNameAndGender(LinkedList List, stopwatch timer)
+        {
+            string name = getUserName();
+            timer.startTimer();
+            Console.WriteLine("Please enter the gender for the person only M or F.");
+            string gender = getUserGender();
+            Console.WriteLine();
+            Node searchedName = List.search(name, gender);
+            timer.stopTimer();
+            if (searchedName == null)
+            {
+                Console.WriteLine("time taken to not find this name was " + timer.outputTime());
+                Console.WriteLine(name + " was not found in our list would you like to add it? \n if so press Y");
+                if (Console.ReadKey().Key == ConsoleKey.Y)
+                {
+                    Console.WriteLine();
+                    userAddName(List, name, timer);
+                }
+            }
+            else
+            {
+                Console.WriteLine("time taken to find this node was" + timer.outputTime());
+                string nameData = searchedName.getUserInfo();
+                Console.WriteLine(nameData);
+            }
+
+        }
         public static void userAddName(LinkedList List, stopwatch timer)
         {
             Console.WriteLine("Please enter the name you would like to add to the list.");
@@ -101,6 +134,7 @@ namespace Boyd_LinkedListSearch
         {
             Console.WriteLine("Please enter the gender for your person only M or F.");
             string gender = getUserGender();
+            Console.WriteLine();
             Console.WriteLine("Please enter how popular the name is using numbers.");
             int popularity = parseinput();
             Node userAdded = List.search(name, gender);
